8ddc554890e56c1d49a64b87c996183322cec240
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-12-20 09:25:20 -0300

    Add files via upload

---
+++ descritiva.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
+db = sns.load_dataset('iris')
+db.head()
+db.shape  #total de amostrs, colunas#
+db['petal_length'].mean()  #média aritmética#
+db['petal_length'].mode()  #moda#
+db['petal_length'].median()  #mediana#
+db['petal_length'].describe()  #Medidas separatrizes#
+sns.boxplot(db['petal_length'])
---
+++ plotdb.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+sns.set_theme(style='darkgrid')
+base_dados = sns.load_dataset('tips')
+base_dados.head()
