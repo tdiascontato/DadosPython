18667a8413f089119e1d75812b992e75292f67bf
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-12-27 12:11:26 -0300

    Update startups.py

--- startups.py
+++ startups.py
@@ -8,9 +8,12 @@ warnings.filterwarnings('ignore')
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_dir, 'startups.csv')
+
 db = pd.read_csv(file_path)
 print(db.shape)
+
 db.head()
+
 db.columns
 db.rename(
     columns={
@@ -26,6 +29,34 @@ db.rename(
 )
 db.info()
 db.isnull().sum()
+
 plt.figure(figsize=(15,6))
 plt.title('Dados Nulos')
 sns.heatmap(db.isnull(), cbar=False)
+
+db.nunique()
+
+db['Empresa'].nunique()
+db['Empresa'].value_counts()
+
+db['Setor'].nunique()
+db['Setor'].value_counts(normalize=True)
+
+plt.figure(figsize=(15,6))
+plt.title('Análise Setores')
+plt.xticks(rotation=45, ha='right')
+plt.bar( db['Setor'].value_counts().index, db['Setor'].value_counts())
+
+db['Pais'].value_counts()
+Analise = round( db['Pais'].value_counts(normalize=True)*100,1)
+top_10_sectors = db['Pais'].value_counts(normalize=True).head(10)
+plt.figure(figsize=(15,6))
+plt.title('Análise Setores')
+plt.pie(
+        top_10_sectors,
+        labels=top_10_sectors.index, # type: ignore
+        shadow=True,
+        startangle=90,
+        autopct='%1.1f%%'
+        );
+
